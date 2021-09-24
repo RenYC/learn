3b68495e5173841f619751cc6be9efb78a43b4a6
ren <1024667257@qq.com>
2021-09-24 15:15:58 +0800

    webpack - 管理输出

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash'
+import printMe from './print';
 
 function component() {
   // 创建一个新的 div 元素
   const element = document.createElement('div')
+  const btn = document.createElement('button');
 
   // lodash（目前通过一个 script 引入）对于指向这一行是必须的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick=printMe;
+
+  element.appendChild(btn)
 
   return element;
 }
