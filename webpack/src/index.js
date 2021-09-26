3f9bd077ec238154d6521a197b14019aa804556f
renyc <ppoo12372@outlook.com>
2021-09-26 23:13:53 +0800

    webpack - 开发环境

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -9,7 +9,7 @@ function component() {
   // lodash（目前通过一个 script 引入）对于指向这一行是必须的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!'
+  btn.innerHTML = 'Click me and check the console!12'
   btn.onclick=printMe;
 
   element.appendChild(btn)
@@ -17,4 +17,5 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // 存储 element，以在 print.js 修改时重新渲染
+document.body.appendChild(element);
