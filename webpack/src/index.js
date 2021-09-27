7280de9a27715ad3ae251475fd87817e012f9657
renyc <ppoo12372@outlook.com>
2021-09-27 09:05:55 +0800

    webpack - 代码分离

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,21 +1,14 @@
-import _ from 'lodash'
-import printMe from './print';
 
-function component() {
+async function getComponent() {
   // 创建一个新的 div 元素
   const element = document.createElement('div')
-  const btn = document.createElement('button');
+  const { default: _ } = await import('lodash')
 
-  // lodash（目前通过一个 script 引入）对于指向这一行是必须的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!12'
-  btn.onclick=printMe;
-
-  element.appendChild(btn)
+  element.innerHTML = _.join(['Hello', 'webpack'], '');
 
   return element;
 }
 
-let element = component(); // 存储 element，以在 print.js 修改时重新渲染
-document.body.appendChild(element);
+getComponent().then(component=>{
+  document.body.appendChild(component)
+})
