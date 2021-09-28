390c8d448182a125b2e785e867ca982ccd6270ed
ren <1024667257@qq.com>
2021-09-28 17:48:54 +0800

    webpack - 初始化Tree Shaking 目录

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,14 +1,13 @@
 import _ from 'lodash';
-// import Print from './print';
 
-function component() {
-  const element = document.createElement('div');
+ function component() {
+   const element = document.createElement('div');
 
-  // lodash 是由当前 script 脚本 import 进来的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.onclick = Print.bind(null, 'Hello webpack!');
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  // lodash 在当前 script 中使用 import 引入
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+   return element;
+ }
 
-document.body.appendChild(component());
+ document.body.appendChild(component());
