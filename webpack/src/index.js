9e78a502d1d9239449170b3c692c861f6ee34529
ren <1024667257@qq.com>
2021-10-08 14:50:43 +0800

    Shimming 预置依赖

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,18 +1,25 @@
-import { cube } from './math.js';
-
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
-}
 
 function component() {
-  const element = document.createElement('pre');
+  const element = document.createElement('div');
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+  // 假设我们处于 `window` 上下文
+  this.alert('Hmmm, this probably isn\'t a great idea...')
 
   return element;
 }
 
 document.body.appendChild(component());
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(
+      "We retrieved some data! AND we're confident it will work on a variety of browser distributions."
+    );
+    console.log(json);
+  })
+  .catch((error) =>
+    console.error('Something went wrong when fetching this data: ', error)
+  );
